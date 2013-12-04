814397ae82736e314fde31d293808b112d6f2bfb
sdsdsd <sdsdsd@dd.dd>
2013-12-04 19:54:43 +0200

    s

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
 $(function(){
-	$('body').append('sdasdasdasd')
+	$('body').append('<div style="position:absolute; width:100%;height:30px;top:0;z-index:9999999;left:0;right:0;">111111111	</div>');
+	$('#headerBlock').remove();
 });
